fdb5d9fc2c64fefc94f57414eaa31b8fb5f731d7
Levi <levi_t@cmu.ac.th>
2026-06-17 12:57:43 +0700

    API Working
    Reconfigured the files to make the API connect. Make sure to update your .env file tho.

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const express = require("express");
+const connection = require("./database");
 
 const app = express();
 
@@ -10,4 +12,63 @@ app.get("/", (req, res) => {
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log("Connected to MySQL");
+    }
+});
+
+app.post("/api/occupancy", (req, res) => {
+
+    const roomId = req.body.roomId;
+    const occupancy = req.body.occupancy;
+
+    const sql = `
+        INSERT INTO occupancy_logs
+        (room_id, occupancy_count)
+        VALUES (?, ?)
+    `;
+
+    connection.query(
+        sql,
+        [roomId, occupancy],
+        (err, result) => {
+
+            if (err) {
+                return res.status(500).json(err);
+            }
+
+            res.json({
+                message: "Occupancy recorded"
+            });
+        }
+    );
+});
+
+app.get("/api/occupancy/history", (req, res) => {
+
+    const sql = `
+        SELECT *
+        FROM occupancy_logs
+        ORDER BY timestamp DESC
+    `;
+
+    connection.query(sql, (err, results) => {
+
+        if (err) {
+            return res.status(500).json(err);
+        }
+
+        res.json(results);
+    });
+});
+
+app.get("/api/test", (req, res) => {
+    res.json({
+        message: "API is working"
+    });
 });
