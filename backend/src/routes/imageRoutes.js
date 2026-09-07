fd4d064506644cc833b6541d7f028cad9581742e
levi <levi_t@cmu.ac.th>
2026-09-07 18:58:48 +0700

    unnecessary stuff removed
    imageRoutes.js was from the prototype

--- backend/src/routes/imageRoutes.js
+++ backend/src/routes/imageRoutes.js
@@ -1,16 +1,16 @@
-const express = require("express");
+// const express = require("express");
 
-const router = express.Router();
+// const router = express.Router();
 
-const upload = require("../middleware/upload");
+// const upload = require("../middleware/upload");
 
-const imageController =
-    require("../controllers/imageController");
+// const imageController =
+    // require("../controllers/imageController");
 
-router.post(
-    "/",
-    upload.single("image"),
-    imageController.uploadImage
-);
+// router.post(
+//     "/",
+//     upload.single("image"),
+//     imageController.uploadImage
+// );
 
-module.exports = router;
+// module.exports = router;
